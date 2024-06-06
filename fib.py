fd513a75c91b49832706b642ee4dc76bb394ec2f
Yoong Jian <yoongjian.how@configura.com>
2024-06-06 14:22:54 +0800

    use pyo3's folder structure

---
+++ fib.py
@@ -0,0 +1,46 @@
+import time
+import random
+
+from python_rust_ffi import fibonacci, get_coordinate, bubble_sort;
+
+def fibonacci_py(nth):
+
+    if nth <= 2:
+        return 1
+    else:
+        return fibonacci(nth - 1) + fibonacci(nth - 2)
+
+print(fibonacci(2))
+
+params = {'x': 1, 'y': 2, 'z': 3}
+print(get_coordinate(params))
+
+unsorted = [random.randint(1, 1000) for _ in range(150)]
+
+num = 55
+
+def py_bubble_sort(nums):
+    nums_copy = nums.copy()
+    n = len(nums_copy)
+    for i in range(n):
+        for j in range(n - i - 1):
+            if nums_copy[j] > nums_copy[j + 1]:
+                nums_copy[j], nums_copy[j + 1] = nums_copy[j + 1], nums_copy[j]
+    return nums_copy
+
+start_time = time.time()
+# fibonacci_py(num)
+py_bubble_sort(unsorted)
+end_time = time.time()
+
+execution_time = end_time - start_time
+print("Python Execution time:", execution_time, "seconds")
+
+
+start_time = time.time()
+# fibonacci(num)
+bubble_sort(unsorted)
+end_time = time.time()
+
+execution_time = end_time - start_time
+print("Rust Execution time:", execution_time, "seconds")
---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use pyo3::prelude::*;
+
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use dict_derive::{FromPyObject, IntoPyObject};
+
+#[derive(Clone, FromPyObject, IntoPyObject)]
+pub struct Point {
+    x: f64,
+    y: f64,
+    z: u64,
+}
+
+#[pyfunction]
+pub fn fibonacci(nth: u64) -> u64 {
+    if nth <= 2 {
+        return 1;
+    }
+
+    fibonacci(nth - 1) + fibonacci(nth - 2)
+}
+
+#[pyfunction]
+pub fn get_coordinate(point: Point) -> PyResult<Point> {
+    Ok(Point {
+        x: point.x + 1.0,
+        y: point.y + 1.0,
+        z: point.z + 1,
+    })
+}
+
+#[pyfunction]
+pub fn bubble_sort(nums: Vec<i32>) -> Vec<i32> {
+    let mut nums_copy = nums.clone();
+    let n = nums_copy.len();
+    for i in 0..n {
+        for j in 0..n-i-1 {
+            if nums_copy[j] > nums_copy[j+1] {
+                nums_copy.swap(j, j+1);
+            }
+        }
+    }
+    nums_copy
+}
+
+#[pymodule]
+fn python_rust_ffi(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(fibonacci, m)?)?;
+    m.add_function(wrap_pyfunction!(get_coordinate, m)?)?;
+    m.add_function(wrap_pyfunction!(bubble_sort, m)?)?;
+    
+
+    Ok(())
+}
